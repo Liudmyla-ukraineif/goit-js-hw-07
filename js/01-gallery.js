91447e7b1e7a3bae2391d66b5ff16b020d360b31
Людмила-ukraineif <cheremisina.ly@gmail.com>
2023-01-11 17:06:57 +0200

    lessons 07/01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,76 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const RefGalleryContainer = document.querySelector('.gallery');
+const cardsGallery = createCardsGallery(galleryItems);
+
+RefGalleryContainer.insertAdjacentHTML('beforeend', cardsGallery);
+
+RefGalleryContainer.addEventListener('click', onCardGalleryClick);
+
+function createCardsGallery(galleryItems){
+  return galleryItems.map(({preview, original, description}) => 
+    { return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `
+}).join('')};
+
+function onCardGalleryClick(e) {
+  e.preventDefault();
+  if(!e.target.classList.contains('gallery__image')){
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <div class="modal">
+    <img src="${e.target.dataset.source}" width="800" height="600">
+  </div>`, 
+  
+  // {
+  //   onShow: (instance) => {
+  //     instance.addEventListener('keydown', onGalleryModalClose);
+  //   },
+  //   onClose: (instance) => {
+  //     instance.removeEventListener('keydown', onGalleryModalClose);
+  //   },
+  // },
+  )
+  instance.show()
+
+  function onGalleryModalClose(e){
+    if(e.code === 'Escape'){
+      instance.close();
+    } 
+  }
+}
+
+
+//   console.log(e.target.dataset.source);
+//   const instance = basicLightbox.create(`
+//     <img src="${e.target.dataset.source}">`, 
+//   {
+//     onShow: (instance) => {
+//       RefGalleryContainer.addEventListener('keydown', onGalleryModalClose);
+//     },
+//     onClose: (instance) => {
+//       RefGalleryContainer.removeEventListener('keydown', onGalleryModalClose);
+//     },
+//   },
+//   instance.show(instance)
+//   );
+
+//   function onGalleryModalClose(e){
+//     if(e.code === 'Escape'){
+//       instance.close();
+//     } 
+//   }
+// }
